c7a66de135913c8dc71f834bd53d491a5480d900
GOKUL KUMAR SANT <gokulsant684@gmail.com>
2025-07-06 23:56:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ n = random.randint(1, 100)
 a = -1
 guesses = 0
 while (a !=n):
-    guesses += 1
+    guesses += 1 
     a = int(input("Enter your guess: "))
     if a < n:
         print("Too low!")
